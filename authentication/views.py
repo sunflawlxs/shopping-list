db4d4f1bceb4bff8e38166583d62d14fe44bd7ca
sunflawlxs <sherylivana99@gmail.com>
2023-11-22 13:45:03 +0700

    tes

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.models import User
 
 @csrf_exempt
 def login(request):
@@ -47,3 +48,16 @@ def logout(request):
         "status": False,
         "message": "Logout gagal."
         }, status=401)
+    
+@csrf_exempt
+def register(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    if User.objects.filter(username=username).exists():
+        return JsonResponse({"status": False, "message": "Username already used."}, status=400)
+
+    user = User.objects.create_user(username=username, password=password)
+    user.save()
+
+    return JsonResponse({"username": user.username, "status": True, "message": "Register successful!"}, status=201)
